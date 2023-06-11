64e1884aa167fc03a95e815904442fbff44ffcc7
Anton Minko <anton.minko@stash.com>
2023-06-11 15:59:26 +0100

    SelectionSort + Benchmark setup fixed

--- src/test/kotlin/SortingTests.kt
+++ src/test/kotlin/SortingTests.kt
@@ -1,18 +1,36 @@
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.collections.shouldBeSorted
 import sorting.bubbleSort
+import sorting.selectionSort
 import utils.deserializeToIntArrays
 import java.io.File
 
 internal class SortingTest : FunSpec({
     val path = "./src/test/resources/testArrays.txt"
-    val testArrays = File(path).deserializeToIntArrays()
+    var testArrays: List<IntArray> = emptyList()
 
-    testArrays.forEach { arr ->
-        test("BubbleSort sorting ${arr.size}") {
-            arr.bubbleSort()
+    context("Bubble sort") {
+        testArrays = File(path).deserializeToIntArrays().take(500).toList()
 
-            arr.toList().shouldBeSorted()
+        testArrays.forEach { arr ->
+            test("Bubble sort ${arr.size}") {
+                arr.bubbleSort()
+
+                arr.toList().shouldBeSorted()
+            }
         }
     }
+
+    context("Selection sort") {
+        testArrays = File(path).deserializeToIntArrays().take(500).toList()
+
+        testArrays.forEach { arr ->
+            test("Selection sort ${arr.size}") {
+                arr.selectionSort()
+
+                arr.toList().shouldBeSorted()
+            }
+        }
+    }
+
 })
